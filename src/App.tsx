ddfe8e8d4d168e6bbd463db4e956961292a067b6
Karalyos Béla <38473508+KaralyosBela@users.noreply.github.com>
2026-05-24 08:45:46 +0000

    remove old code, use hook instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import { useEffect, useState } from "react";
-// import { supabase } from "./utils/util";
-// import { useUser } from "./useGetUser";
-
-// export default function App() {
-//   const user = useUser();
-//   console.log(user);
-//   const [data, setData] = useState<any>([]);
-//   useEffect(() => {
-//     async function getTodos() {
-//       const { data } = await supabase.from("foods").select();
-//       setData(data);
-//       console.log(data);
-//     }
-
-//     getTodos();
-//   }, []);
-
-//   useEffect(() => {
-//     supabase.auth.getSession().then(({ data }) => {
-//       console.log("session:", data.session);
-//     });
-
-//     const { data: listener } = supabase.auth.onAuthStateChange(
-//       (_event, session) => {
-//         console.log("auth changed:", session);
-//       },
-//     );
-
-//     return () => listener.subscription.unsubscribe();
-//   }, []);
-
-//   //NAPI
-//   // supabase
-//   // .from("food_logs")
-//   // .select("calories, protein")
-//   // .eq("user_id", user.id)
-//   // .gte("eaten_at", "2026-05-23")
-
-//   //HETI
-//   // supabase
-//   // .from("food_logs")
-//   // .select("*")
-//   // .gte("eaten_at", startOfWeek)
-
-//   return (
-//     <>
-//       asd
-//       {data.map((d: any) => {
-//         return <p>{d.name}</p>;
-//       })}
-//     </>
-//   );
-// }
-
-import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
-import { supabase } from "./database/supabase";
 import Auth from "./components/Auth";
+import { useAuth } from "./hooks/useAuth";
+import { supabase } from "./database/supabase";
 
 export default function App() {
-  console.log(import.meta.env);
-  const [session, setSession] = useState<any>(null);
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data }) => {
-      setSession(data.session);
-    });
-
-    const { data: listener } = supabase.auth.onAuthStateChange(
-      (_event, session) => {
-        setSession(session);
-      },
-    );
-
-    return () => listener.subscription.unsubscribe();
-  }, []);
+  const session = useAuth();
 
   if (!session) return <Auth />;
 
